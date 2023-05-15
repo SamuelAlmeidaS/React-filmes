0093b70a522d326424e9d4ef4986e2bce023fdd6
Samuel Almeida <75258537+SamuelAlmeidaS@users.noreply.github.com>
2023-05-15 16:23:06 -0300

    add layout loading

---
+++ src/components/loading.jsx
@@ -0,0 +1,11 @@
+import style from './loading.module.css';
+
+export const Loading = () => {
+    return (
+        <div className={style.screen}>
+            <div>
+                <h1>Carregando....</h1>
+            </div>
+        </div>
+    )
+};
