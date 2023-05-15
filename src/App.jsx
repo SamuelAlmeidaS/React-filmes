a01fb900c164df38c5aebd7c10540257dc0cd392
Samuel Almeida <75258537+SamuelAlmeidaS@users.noreply.github.com>
2023-05-15 16:24:48 -0300

    add component Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,49 @@
 import { useState, useEffect } from 'react';
+import { Loading } from './components/loading'
 import style from './App.module.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => { handleCLick() }, []);
 
   const handleCLick = async () => {
+    setLoading(true);
     let response = await fetch('https://api.b7web.com.br/cinema/');
     let json = await response.json();
+    setLoading(false);
     setMovies(json);
   }
 
   return (
     <div className={style.main}>
-      <div className={style.header}>
-          <h1>Filmes em cartaz</h1>
-      </div>
+      {loading && <Loading />}
 
-      {/* <button classNmae={style.button} onClick={handleCLick}>Click aqui</button>
-      Total de filmes: {movies.length} */}
-
-      <div className={style.grid}>
-
-        {movies.map((item, index) => (
-          <div key={index} className={style.flex}>
-
-            <img src={item.avatar} alt="" className={style.width} />
-
-            <div className={style.title}>
-
-              {item.titulo}
-
-            </div>
+      {!loading &&
+        <>
+          <div className={style.header}>
+            <h1>Filmes em cartaz</h1>
           </div>
-        ))}
 
-      </div>
+          <div className={style.grid}>
+
+            {movies.map((item, index) => (
+              <div key={index} className={style.flex}>
+
+                <img src={item.avatar} alt="" className={style.width} />
+
+                <div className={style.title}>
+
+                  {item.titulo}
+
+                </div>
+              </div>
+            ))}
+
+          </div>
+        </>
+      }
     </div>
   );
 }
